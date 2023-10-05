cd4348f2c0eec1a5d6cbe2aa1c04a19b3a559e86
KyleDV <113366907+KyleDV@users.noreply.github.com>
2023-10-06 01:17:12 +0800

    functionsanderrors.sol

--- functionsanderrors.sol
+++ functionsanderrors.sol
@@ -1,12 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
- /**
-   * @title ContractName
-   * @dev ContractDescription
-   * @custom:dev-run-script compiler_config.json
-   */
-
 contract MyToken {
 
     // public variables here
