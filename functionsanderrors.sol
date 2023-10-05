ccd204b738de521c6b61e829ae2cf31127f2e6e5
KyleDV <113366907+KyleDV@users.noreply.github.com>
2023-10-06 01:15:47 +0800

    Add files via upload

---
+++ functionsanderrors.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+ /**
+   * @title ContractName
+   * @dev ContractDescription
+   * @custom:dev-run-script compiler_config.json
+   */
+
+contract MyToken {
+
+    // public variables here
+    string public tokenname = "POG Coin";
+    string public tokenabbrv = "pc";
+    uint public totalsupply = 0;
+
+    // mapping variable here
+    mapping(address => uint) public balance;
+ 
+    // mint function
+    function mint(address addr, uint val) public{
+        totalsupply += val;
+        balance[addr] += val;
+    }
+
+    //Test Burn function
+    function testburn(address addr, uint val) public{
+        require(balance[addr] >= val, "Insufficient Balance");
+        totalsupply -= val;
+        balance[addr] -= val;
+        revert("Enough Balance");
+    }
+
+    // burn function
+    function burn(address addr, uint val) public{
+        assert(balance[addr] >= val);
+        totalsupply -= val;
+        balance[addr] -= val;
+    }
+
+}
